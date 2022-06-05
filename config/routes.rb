e81088271d2261522cc8ed747ed38a278af53a64
rikifujihara <rikifujihara@gmail.com>
2022-06-05 14:03:03 +1000

    cleaned up routes file for Users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
-  get 'users/edit'
   devise_for :users
+  resources :users, only: [:index, :show, :edit, :update]
   get 'pages/home'
   root 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
