23ca64fa7d81fc6da619f4dbb8c96e83444baf13
rikifujihara <rikifujihara@gmail.com>
2022-06-06 17:05:23 +1000

    added delete image method

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_one_attached :profile_picture, dependent: :destroy
+  validates :profile_picture, content_type: [:png, :jpg, :jpeg]
+  
+
   
   def full_name
     "#{first_name} #{last_name}" 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users, only: [:index, :show, :edit, :update]
+  resources :users, only: [:index, :show, :edit, :update] do
+    member do
+     delete 'delete_image/:image_id', action: 'delete_image', as: 'delete_image'
+    end
+  end
   get 'pages/home'
   root 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
