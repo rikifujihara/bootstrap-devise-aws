b65cec1add409fddee8cf977e3aa472b935e24eb
rikifujihara <rikifujihara@gmail.com>
2022-06-06 16:42:17 +1000

    added some users to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,22 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+  users = [
+      { email: 'thomas@example.com', first_name: 'Thomas', last_name: 'Bush', admin: true },
+      { email: 'bear@example.com', first_name: 'Bear', last_name: 'Bush', admin: false }
+  ]
+
+  users.each do |user|
+    this_user = User.where(
+        email: user[:email],
+    ).first_or_initialize
+
+    this_user.update!(
+        first_name: user[:first_name],
+        last_name: user[:last_name],
+        admin: user[:admin],
+        password: SecureRandom.uuid
+    )
+    this_user.save
+  end
+
